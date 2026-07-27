9be122037ed196d1aa81b1f5e9146774cac26786
healthiervietinbank-app <healthiervietinbank@gmail.com>
2026-07-27 14:47:09 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    noDiscovery: true,
-    include: [],
-  },
   server: {
     port: 4173,
   },
